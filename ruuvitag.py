5bcfed568f6eeba799969f29f22fbe2aad55b860
MrGeCode <niko@vaatturi.fi>
2023-03-19 20:25:22 +0200

    ruuvitag.py testing

---
+++ ruuvitag.py
@@ -0,0 +1,9 @@
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+
+
+def handle_data(found_data):
+    print(f"MAC {found_data[0]}")
+    print(f"Data {found_data[1]}")
+
+if __name__ == "__main__":
+    RuuviTagSensor.get_data(handle_data)

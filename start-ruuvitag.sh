426e02cfe0203643204d7d2cc030b453e0635159
MrGeCode <niko@vaatturi.fi>
2023-03-21 13:38:11 +0200

    start-ruuvitag.sh service

---
+++ start-ruuvitag.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# activate the virtual environment
+source /home/nikopelkonen/workspace/homescreen/bienv/bin/activate
+
+# run the ruuvitag.py script
+python /home/nikopelkonen/workspace/homescreen/ruuvitag.py

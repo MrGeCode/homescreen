20dd48fb8e0bee57a18d4d987c32f530d3d69a8e
MrGeCode <niko@vaatturi.fi>
2023-03-28 11:10:47 +0300

    start service

--- start-ruuvitag.sh
+++ start-ruuvitag.sh
@@ -5,4 +5,4 @@ source /home/nikopelkonen/workspace/homescreen/bienv/bin/activate
 
 # run the ruuvitag.py script
 python /home/nikopelkonen/workspace/homescreen/ruuvitag.py
-python /home/nikopelkonen/workspace/homescreen/temperature/printtemptosjson.py
+python /home/nikopelkonen/workspace/homescreen/temperature/printtemptojson.py

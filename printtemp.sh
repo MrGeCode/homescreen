bf723a05b9e787fd3ca2640d2f62e69df1043dc4
MrGeCode <niko@vaatturi.fi>
2023-04-04 13:08:06 +0300

    new brach

--- printtemp.sh
+++ printtemp.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# activate the virtual environment
+source /home/nikopelkonen/workspace/homescreen/bienv/bin/activate
+
 python /home/nikopelkonen/workspace/homescreen/temperature/printtemptojson.py
